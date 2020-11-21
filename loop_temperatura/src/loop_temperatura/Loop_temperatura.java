54e66af7b5de645b3db08dfc91ba9e0ae46a8e55
victrslx <joaoslx1@gmail.com>
2020-11-21 20:13:12 -0300

    loop_temperatura

---
+++ loop_temperatura/src/loop_temperatura/Loop_temperatura.java
@@ -0,0 +1,21 @@
+package loop_temperatura;
+import java.util.Scanner;
+public class Loop_temperatura {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        double c, f, a;
+        int i;
+        System.out.print("Digite o valor em celcius: ");
+        c = entrada.nextDouble();
+        f = (c*1.8+32);
+        System.out.print("Valor da converção é : "+f);
+        i = 10;
+        while (1<=100){
+        a = f+1;
+        System.out.println(a);
+        i =i+10;       
+        }
+    }
+    
+}
